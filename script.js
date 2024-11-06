96eb623d38cdf39c197e003cadd36689d41150aa
DomenicoLopez <84095064+DomenicoLopez@users.noreply.github.com>
2024-11-06 13:46:39 -0500

    put everything into script.js

--- script.js
+++ script.js
@@ -11,7 +11,27 @@ document.addEventListener('DOMContentLoaded', () => {
     String message = document.getElementById('message').value;
 
     // Display a confirmation message
-    confirmationMessage.textContent = parse(name);
+    String userName = input;
+    int len = userName.length();
+    String bbd = userName.substring(len-6,len);
+    userName = userName.substring(2,len-8);
+    int indexSCCCPN = userName.indexOf("2400000000000000");
+    String SSCC = userName.substring(0,indexSCCCPN);
+    userName = userName.substring(indexSCCCPN+16);
+    String PN = userName.substring(0,5);
+    userName = userName.substring(5);
+    if (!userName.substring(0,2).equalsIgnoreCase("10")){
+        int indexPNBatch = userName.indexOf("10");
+        String PNremainder = userName.substring(0,indexPNBatch);
+        PN = PN + PNremainder;
+        userName = userName.substring(indexPNBatch+2);
+    }
+    else{
+        userName = userName.substring(2);
+    }
+    String batch = userName;
+    String refactoring = "]d" + SSCC + "240" + PN + "~10" + batch + "~17" + bbd;
+    confirmationMessage.textContent = `Thank you, ${refactoring}! We have received your message.`;
 
     // Reset the form
     form.reset();
