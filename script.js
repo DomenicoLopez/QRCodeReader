c283fdefb707e3adf5fa09f86ebc11ecb4e32bda
DomenicoLopez <84095064+DomenicoLopez@users.noreply.github.com>
2024-11-14 03:34:41 -0500

    modify jscript

--- script.js
+++ script.js
@@ -5,31 +5,40 @@ document.addEventListener('DOMContentLoaded', () => {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
 
+//    // Gather form data
+//    const userName = document.getElementById('name').value;
+//    const email = document.getElementById('email').value;
+//    const message = document.getElementById('message').value;
+//
+//    // Display a confirmation message
+//    const bbd = userName.slice(-6);
+//    userName = userName.slice(2,-8);
+//    int indexSCCCPN = userName.indexOf("2400000000000000");
+//    const SSCC = userName.slice(0,indexSCCCPN);
+//    userName = userName.slice(indexSCCCPN+16);
+//    String PN = userName.slice(0,5);
+//    userName = userName.slice(5);
+//    if (!userName.slice(0,2).localeCompare("10")){
+//        int indexPNBatch = userName.indexOf("10");
+//        const PNremainder = userName.slice(0,indexPNBatch);
+//        PN = PN + PNremainder;
+//        userName = userName.slice(indexPNBatch+2);
+//    }
+//    else{
+//        userName = userName.slice(2);
+//    }
+//    const batch = userName;
+//    const refactoring = "]d" + SSCC + "240" + PN + "~10" + batch + "~17" + bbd;
+//    confirmationMessage.textContent = `Thank you, ${refactoring}! We have received your message.`;
+
     // Gather form data
-    const userName = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+    const PN = document.getElementById('PN').value;
+    const batch = document.getElementById('Batch').value;
+    const BBD = document.getElementById('BBD').value;
+    const SSCC = document.getElementById('SSCC').value;
 
     // Display a confirmation message
-    const bbd = userName.slice(-6);
-    userName = userName.slice(2,-8);
-    int indexSCCCPN = userName.indexOf("2400000000000000");
-    const SSCC = userName.slice(0,indexSCCCPN);
-    userName = userName.slice(indexSCCCPN+16);
-    String PN = userName.slice(0,5);
-    userName = userName.slice(5);
-    if (!userName.slice(0,2).localeCompare("10")){
-        int indexPNBatch = userName.indexOf("10");
-        const PNremainder = userName.slice(0,indexPNBatch);
-        PN = PN + PNremainder;
-        userName = userName.slice(indexPNBatch+2);
-    }
-    else{
-        userName = userName.slice(2);
-    }
-    const batch = userName;
-    const refactoring = "]d" + SSCC + "240" + PN + "~10" + batch + "~17" + bbd;
-    confirmationMessage.textContent = `Thank you, ${refactoring}! We have received your message.`;
+    confirmationMessage.textContent = `]d${SSCC}240${PN}~10${batch}~17${BBD}`;
 
     // Reset the form
     form.reset();
